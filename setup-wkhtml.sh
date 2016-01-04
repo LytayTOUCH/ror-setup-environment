80610fc4796f8bd9352108dcd74b921e7e3c91df
Lytay TOUCH <lytaytouch@gmail.com>
2016-01-04 14:47:54 +0700

    [Edit] Update setup wkthml script to remove downloaded package

--- setup-wkhtml.sh
+++ setup-wkhtml.sh
@@ -10,10 +10,13 @@ sudo apt-get upgrade -y
 
 echo -e "---- Install wkhtml ----"
 sudo wget http://download.gna.org/wkhtmltopdf/0.12/0.12.2.1/wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb
-sudo dpkg -i wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb
 sudo apt-get -f install -y
+sudo dpkg -i wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb
 
 echo -e "---- Update wkhtml directory ----"
 sudo cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 sudo cp /usr/local/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
+
+echo -e "---- Remove wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb ----"
+sudo rm wkhtmltox-0.12.2.1_linux-wheezy-amd64.deb
