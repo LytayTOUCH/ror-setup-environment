c52b22ee789d8e2ee6fd7205fcaf0feeba587a34
LytayTOUCH <lytaytouch@gmail.com>
2019-02-11 01:07:49 +0700

    Correct nvm version

--- setup-nvm.sh
+++ setup-nvm.sh
@@ -4,7 +4,7 @@
 #Install NVM for nodejs package manager
 #-----------------------------------------
 echo -e "---- Install NVM for Nodejs Package Manager Into Server ----"
-NVM_VERSION="0.34.0"
+NVM_VERSION="v0.34.0"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | bash
 source ~/.profile
 echo -e "---- Install NodeJS and IOJS Into NVM ----"
